86c0585a9094077164230804e1cc425d984008b0
kai <kai.kesselring@jpberlin.de>
2014-02-15 00:47:22 +0100

    added test

---
+++ MyTest.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: kai
+ * Date: 15.02.14
+ * Time: 00:36
+ */
+
+class MyTest extends PHPUnit_Framework_TestCase {
+
+    public function testFalse()
+    {
+        $this->assertTrue(false);
+    }
+
+    public function testTrue()
+    {
+        $this->assertTrue(true);
+    }
+
+} 
